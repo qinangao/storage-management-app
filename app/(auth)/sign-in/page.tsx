d055dc5ffe58981ecb6bd3f772230e2d35e59f1e
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-28 19:16:41 +0100

    add sign-in and sign-up pages with AuthForm component

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+import AuthForm from "@/components/AuthForm";
+
+function SignIn() {
+  return <AuthForm type="sign-in" />;
+}
+
+export default SignIn;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import AuthForm from "@/components/AuthForm";
+
+function SignUp() {
+  return <AuthForm type="sign-up" />;
+}
+
+export default SignUp;
