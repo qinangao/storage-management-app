4c0b459cb5beda88fcfe0584df2836db6f750b2e
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-28 22:44:56 +0100

    Initialize Appwrite server API endpoint

---
+++ lib/index.ts
@@ -0,0 +1,47 @@
+import { Account, Avatars, Client, Databases, Storage } from "node-appwrite";
+import { appwriteConfig } from "./config";
+import { cookies } from "next/headers";
+
+// session client:will to a specific user session, let user access their data and perform they are allow to
+export async function createSessionClient() {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projrctId);
+
+  const session = (await cookies()).get("appwrite-session");
+
+  if (!session || !session.value) {
+    throw new Error("No session");
+  }
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+  };
+}
+// admin client: create instance with admin level permission to manage entir app write project,only use it in the server
+export async function createAdminClient() {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projrctId)
+    .setKey(appwriteConfig.secretKey);
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+    get storage() {
+      return new Storage(client);
+    },
+    get avatar() {
+      return new Avatars(client);
+    },
+  };
+}
