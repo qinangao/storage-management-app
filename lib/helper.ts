c6f541fda718260a9d1008595a80f03ab61899cd
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-30 22:55:36 +0100

    refactor: remove unused user-related functions and retain error handling

--- lib/helper.ts
+++ lib/helper.ts
@@ -1,29 +1,4 @@
-import { ID, Query } from "node-appwrite";
-import { createAdminClient } from ".";
-import { appwriteConfig } from "./config";
-
-export async function getUserByEmail(email: string) {
-  const { databases } = await createAdminClient();
-  const result = await databases.listDocuments(
-    appwriteConfig.databaseId,
-    appwriteConfig.usersCollectionId,
-    [Query.equal("email", email)]
-  );
-  return result.total > 0 ? result.documents[0] : null;
-}
-
-function handleError(error: unknown, message: string) {
+export function handleError(error: unknown, message: string) {
   console.log(error, message);
   throw error;
-}
-
-export async function sendEmailOTP({ email }: { email: string }) {
-  const { account } = await createAdminClient();
-  try {
-    const session = await account.createEmailToken(ID.unique(), email);
-
-    return session.userId;
-  } catch (error) {
-    handleError(error, "Fail to send email OTP");
-  }
 }
