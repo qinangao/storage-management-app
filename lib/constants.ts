435b2034203d42c0f16bcd074d55f142602589db
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-30 22:54:48 +0100

    feat: add navigation items and avatar placeholder URL constants

---
+++ lib/constants.ts
@@ -0,0 +1,14 @@
+export const navItems = [
+  { name: "Dashboard", icons: "/assets/icons/dashboard.svg", url: "/" },
+  {
+    name: "Documents",
+    icons: "/assets/icons/documents.svg",
+    url: "/documents",
+  },
+  { name: "Images", icons: "/assets/icons/images.svg", url: "/images" },
+  { name: "Media", icons: "/assets/icons/video.svg", url: "/media" },
+  { name: "Others", icons: "/assets/icons/others.svg", url: "/others" },
+];
+
+export const avatarPlaceholderUrl =
+  "https://img.freepik.com/premium-vector/profile-picture-placeholder-avatar-silhouette-gray-tones-icon-colored-shapes-gradient_1076610-40164.jpg";
