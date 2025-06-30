c8b3fa68fe0d2883cef2a0f1e9562fee854f795c
qinanagao@gmail.com <qinanagao@gmail.com>
2025-06-30 22:52:15 +0100

    add image remote patterns to next.config for external image support

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cloud.appwrite.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
