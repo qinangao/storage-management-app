6196e8999d57cfb4c7d906462153eb3460bdef59
qinanagao@gmail.com <qinanagao@gmail.com>
2025-07-01 15:21:20 +0100

    feat: implement FileUploader component with drag-and-drop functionality

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,5 +1,94 @@
-function FileUploader() {
-  return <div>FileUploader</div>;
+"use client";
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { Button } from "./ui/button";
+import { cn, convertFileToUrl, getFileType } from "@/lib/utils";
+import Image from "next/image";
+import Thumbnail from "./Thumbnail";
+
+type FileUploaderPropsType = {
+  ownerId: string;
+  accountId: string;
+  className: string;
+};
+
+function FileUploader({
+  ownerId,
+  accountId,
+  className,
+}: FileUploaderPropsType) {
+  const [files, setFiles] = useState<File[]>([]);
+  const onDrop = useCallback(async (acceptedFiles: File[]) => {
+    setFiles(acceptedFiles);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  function handleRemoveFile(
+    e: React.MouseEvent<HTMLImageElement, MouseEvent>,
+    fileName: string
+  ) {
+    e.stopPropagation();
+    setFiles((prevFile) => prevFile.filter((file) => file.name !== fileName));
+  }
+  return (
+    <div {...getRootProps()} className="cursor-point">
+      <input {...getInputProps()} />
+      <Button type="button" className={cn("uploader-button", className)}>
+        <Image
+          src="/assets/icons/upload.svg"
+          alt="upload"
+          width={24}
+          height={24}
+        />{" "}
+        <p>Upload</p>
+      </Button>
+      {files.length > 0 && (
+        <ul className="uploader-preview-list">
+          <h4 className="h4 text-light-100">Uploading</h4>
+          {files.map((file, index) => {
+            const { type, extension } = getFileType(file.name);
+            return (
+              <li
+                key={`${file.name}-${index}`}
+                className="uploader-preview-item"
+              >
+                <div className="flex items-center gap-3">
+                  <Thumbnail
+                    type={type}
+                    extension={extension}
+                    url={convertFileToUrl(file)}
+                  />
+
+                  <div className="preview-item-name">
+                    {file.name}
+                    <Image
+                      src="/assets/icons/file-loader.gif"
+                      width={80}
+                      height={26}
+                      alt="loader"
+                      className="mt-3"
+                    />
+                  </div>
+                </div>
+                <Image
+                  src="/assets/icons/remove.svg"
+                  width={24}
+                  height={24}
+                  alt="remove"
+                  onClick={(e) => handleRemoveFile(e, file.name)}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      )}
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )}
+    </div>
+  );
 }
 
 export default FileUploader;
