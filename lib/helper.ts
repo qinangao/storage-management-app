ade398a1396b9cf7d5a883133646e000da093988
qinanagao@gmail.com <qinanagao@gmail.com>
2025-07-01 14:06:36 +0100

    feat: add getInitials function to extract the first initial from a full name

--- lib/helper.ts
+++ lib/helper.ts
@@ -2,3 +2,12 @@ export function handleError(error: unknown, message: string) {
   console.log(error, message);
   throw error;
 }
+
+export function getInitials(fullName: string) {
+  if (!fullName) return "";
+  const words = fullName.trim().split(" ");
+  // More than one word, take first letter of first and last word
+  const firstInitial = words[0][0].toUpperCase();
+
+  return firstInitial;
+}
