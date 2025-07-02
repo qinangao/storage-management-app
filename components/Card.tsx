6221644aa1bc6c53f2de83ae1052f5ecb5cea09b
qinanagao@gmail.com <qinanagao@gmail.com>
2025-07-02 23:14:31 +0100

    feat: add ActionDropdown component to Card for file actions

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,6 +3,7 @@ import { Models } from "node-appwrite";
 import Thumbnail from "./Thumbnail";
 import { convertFileSize } from "@/lib/utils";
 import FormattedDateTime from "./FormattedDateTime";
+import ActionDropdown from "./ActionDropdown";
 
 function Card({ file }: { file: Models.Document }) {
   return (
@@ -16,7 +17,7 @@ function Card({ file }: { file: Models.Document }) {
           imageClassName="!size-11"
         />
         <div className="flex flex-col items-end justify-between">
-          Actions dropdown
+          <ActionDropdown file={file} />
           <p className="body-1">{convertFileSize(file.size)}</p>
         </div>
       </div>
