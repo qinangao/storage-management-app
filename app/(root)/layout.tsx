04e51c2b02b521d910d56ead5074ecd88f1677d0
qinanagao@gmail.com <qinanagao@gmail.com>
2025-07-07 18:49:38 +0100

    add "force-dynamic" r to control the rendering behavior

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,6 +6,8 @@ import { redirect } from "next/navigation";
 import { Toaster } from "@/components/ui/sonner";
 import React from "react";
 
+export const dynamic = "force-dynamic";
+
 async function layout({ children }: { children: React.ReactNode }) {
   const currentUser = await getCurrentUser();
 
