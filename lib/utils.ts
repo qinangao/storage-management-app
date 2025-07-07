7eea7a1e19e85b1a29991ca1a9ac03f3a35dd685
qinanagao@gmail.com <qinanagao@gmail.com>
2025-07-07 18:50:15 +0100

    import type for filetype

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { appwriteConfig } from "./config";
+import { FileType } from "@/types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
