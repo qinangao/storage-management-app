35491fdc47ed6ef8916b014c670934741a92e9f9
qinanagao@gmail.com <qinanagao@gmail.com>
2025-07-07 11:27:01 +0100

    impletment search functionality

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,5 +1,109 @@
+"use client";
+import Image from "next/image";
+import { Input } from "./ui/input";
+import { useEffect, useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { getFiles } from "@/lib/actions/file.action";
+import { Models } from "node-appwrite";
+import Thumbnail from "./Thumbnail";
+import FormattedDateTime from "./FormattedDateTime";
+import { useDebounce } from "use-debounce";
+
 function Search() {
-  return <div>Search</div>;
+  const [query, setQuery] = useState("");
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get("query") || "";
+  const [results, setResults] = useState<Models.Document[]>([]);
+  const [open, setOpen] = useState(false);
+
+  const router = useRouter();
+  const path = usePathname();
+  const [debouncedQuery] = useDebounce(query, 300);
+
+  useEffect(() => {
+    async function fetchFiles() {
+      if (debouncedQuery.length === 0) {
+        setResults([]);
+        setOpen(false);
+        return router.push(path.replace(searchParams.toString(), ""));
+      }
+      const files = await getFiles({ types: [], searchText: debouncedQuery });
+
+      setResults(files.documents);
+      setOpen(true);
+    }
+
+    fetchFiles();
+  }, [debouncedQuery, router, path, searchParams]);
+
+  useEffect(() => {
+    if (!searchQuery) {
+      setQuery("");
+    }
+  }, [searchQuery]);
+
+  function handleClickItem(file: Models.Document) {
+    setOpen(false);
+    setResults([]);
+    router.push(
+      `/${
+        file.type === "video" || file.type === "audio"
+          ? "media"
+          : file.type + "s"
+      }?query=${query}`
+    );
+  }
+
+  return (
+    <div className="search">
+      <div className="search-input-wrapper">
+        <Image
+          src="/assets/icons/search.svg"
+          alt="search"
+          width={24}
+          height={24}
+        />
+        <Input
+          value={query}
+          placeholder="Search..."
+          className="search-input"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        {open &&
+          (results.length > 0 ? (
+            <ul className="search-result">
+              {results.map((file) => (
+                <li
+                  key={file.$id}
+                  className="flex items-center justify-between"
+                  onClick={() => handleClickItem(file)}
+                >
+                  <div className="flex cursor-pointer items-center gap-4">
+                    <Thumbnail
+                      type={file.type}
+                      extension={file.extension}
+                      url={file.url}
+                      className="size-9 min-w-9"
+                    />
+                    <p className="subtitle-2 line-clamp-1 text-light-100">
+                      {file.name}
+                    </p>
+                  </div>
+                  <FormattedDateTime
+                    date={file.$createdAt}
+                    className="caption line-clamp-1 text-light-200"
+                  />
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div className="search-result">
+              <p className="empty-result">No results found</p>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 }
 
 export default Search;
